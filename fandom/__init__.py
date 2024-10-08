b6e45d670cb3f0ec6a228344bdfac8a65eb0956e
NgThVinh <ngthevinh06@gmail.com>
2024-10-08 15:46:58 +0700

    Refactor __init__.py to include set_ignored_elements in __all__

--- fandom/__init__.py
+++ fandom/__init__.py
@@ -3,4 +3,4 @@ from .fandom import default_url, page, random, search, set_lang, set_rate_limiti
 
 __version__ = (0, 2, 1)
 
-__all__ = ["default_url", "page", "random", "search", "set_lang", "set_rate_limiting", "set_wiki", "set_user_agent", "summary"]
+__all__ = ["default_url", "page", "random", "search", "set_lang", "set_rate_limiting", "set_wiki", "set_user_agent", "set_ignored_elements", "summary"]
